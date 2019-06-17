fa59006ad0cef85920d90aed589247bc3ff413dc
jp aulet <blakdrak@gmail.com>
2019-06-17 13:35:42 +0200

    Deleted unused routes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,4 @@ Route::get('/', function () { return view('landing'); })->name('landing');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/ideas', 'HomeController@ideas')->name('ideas');
-
-Route::get('/blog/{slug?}', function($slug = null) {
-	return File::get(public_path().'/blog/dist/'.$slug);
-});
+Route::view('/ideas', 'ideas')->name('ideas');
