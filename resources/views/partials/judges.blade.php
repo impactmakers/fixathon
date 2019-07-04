3c2f4241f733475061c7f4e91622c67a876f202c
shylands <steve@shylands.com>
2019-07-04 11:53:02 +0200

    finishing judges section markup and style

--- resources/views/partials/judges.blade.php
+++ resources/views/partials/judges.blade.php
@@ -17,20 +17,96 @@
         <img alt="Simon Collison" class="judge__img" src="{{asset('img/people/simon.jpg')}}">
         <h3>Simon Collison</h3>
         <p>Designer, writer, occasional speaker. Director of New Adventures @naconf.</p>
-        <a href='https://twitter.com/colly' target='_blank' class=''>@colly</a>  
-      </li>
-      <li class="judges__judge">
-        <img alt="Michelle Thorne" class="judge__img" src="{{asset('img/people/michelle.jpg')}}">
-        <h3>Michelle Thorne</h3>
-        <p>Senior Program Officer at the Mozilla Foundation. Currently interested in making the internet carbon-neutral.</p>
-        <a href='https://twitter.com/thornet' target='_blank' class=''>@thornet</a>
+        <a href='https://twitter.com/colly' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @colly</a>  
       </li>
       <li class="judges__judge">
         <img alt="Anne-Laure Le Cunff" class="judge__img" src="{{asset('img/people/anne.jpg')}}">
         <h3>Anne-Laure Le Cunff</h3>
-        <p>Founder @ness_labs • Running @makermag • msc #neuroscience candidate • scout @BackedVC • ex @google • creator of #diversity30.</p>
-        <a href='https://twitter.com/anthilemoon' target='_blank' class=''>@anthilemoon</a>
+        <p>Founder @ness_labs • Running @makermag • ex @google • creator of #diversity30.</p>
+        <a href='https://twitter.com/anthilemoon' target='_blank' class='judge__link'>
+          <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @anthilemoon</a>
       </li>  
+      
+
+      <li class="judges__judge">
+        <img alt="Justin Jackson" class="judge__img" src="{{asset('img/people/justin.jpg')}}">
+        <h3>Justin Jackson</h3>
+        <p>Working with SaaS companies since 2008. Building Transistor.fm.</p>
+        <a href='https://twitter.com/mijustin' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @mijustin</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="Veni Kunche" class="judge__img" src="{{asset('img/people/veni.jpg')}}">
+        <h3>Veni Kunche</h3>
+        <p>Short bio about why they make a good judge for this competition.</p>
+        <a href='https://twitter.com/venikunche' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @venikunche</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="Cennydd Bowles" class="judge__img" src="{{asset('img/people/cennydd.jpg')}}">
+        <h3>Cennydd Bowles</h3>
+        <p>Short bio about why they make a good judge for this competition.</p>
+        <a href='https://twitter.com/cennydd' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @cennydd</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="Charli Prangley" class="judge__img" src="{{asset('img/people/charli-prangley.jpg')}}">
+        <h3>Charli Prangley</h3>
+        <p>Short bio about why they make a good judge for this competition.</p>
+        <a href='https://twitter.com/charliprangley' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @charliprangley</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="James Singleton" class="judge__img" src="{{asset('img/people/james.jpg')}}">
+        <h3>James Singleton</h3>
+        <p>Short bio about why they make a good judge for this competition.</p>
+        <a href='https://twitter.com/shutdownscanner' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @shutdownscanner</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="Michelle Thorne" class="judge__img" src="{{asset('img/people/michelle.jpg')}}">
+        <h3>Michelle Thorne</h3>
+        <p>Michelle leads Mozilla’s Open Internet of Things Studio.</p>
+        <a href='https://twitter.com/thornet' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @thornet</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="Fajar Siddiq" class="judge__img" src="{{asset('img/people/fajar.jpg')}}">
+        <h3>Fajar Siddiq</h3>
+        <p>Short bio about why they make a good judge for this competition.</p>
+        <a href='https://twitter.com/fajarsiddiqfs' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @fajarsiddiqfs</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="Charlie Gerard" class="judge__img" src="{{asset('img/people/charlie-gerard.jpg')}}">
+        <h3>Charlie Gerard</h3>
+        <p>Short bio about why they make a good judge for this competition.</p>
+        <a href='https://twitter.com/devdevcharlie' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @devdevcharlie</a>
+      </li>
+      <li class="judges__judge">
+        <img alt="Catherine Friend" class="judge__img" src="{{asset('img/people/catherine.jpg')}}">
+        <h3>Catherine Friend</h3>
+        <p>Short bio about why they make a good judge for this competition.</p>
+        <a href='https://twitter.com/1password' target='_blank' class='judge__link'>
+        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}">
+        @1password</a>
+      </li>
+
+
+
+
     </ul>
   </div>
 </section>
