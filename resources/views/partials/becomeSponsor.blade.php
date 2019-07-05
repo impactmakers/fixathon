d3c7cb61a2e36705858c94766b5d6bf53f399adc
shylands <steve@shylands.com>
2019-07-05 07:58:39 +0200

    intial setup of sponsors and entries sections

--- resources/views/partials/becomeSponsor.blade.php
+++ resources/views/partials/becomeSponsor.blade.php
@@ -1,22 +1,12 @@
 <!-- BECOME SPONSOR -->
-<section class="section section-lg mb-5 pb-2">
-  <div class="container">
-    <div class="card bg-gradient-success shadow-lg border-0">
-      <div class="p-5">
-        <div class="row align-items-center">
-          <div class="col-lg-8">
-            <h3 class="text-white">We made the world better with you.</h3>
-            <p class="lead text-white mt-3">Support all incredible impact makers by sponsoring this hackathon.</p>
-          </div>
-          <div class="col-lg-3 ml-lg-auto">
-            <a href="./sponsors" class="btn btn-lg btn-block btn-white">Sponsor</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="become-donor">
-      Our become a donor 💚 <a href="" class="btn btn-success ml-3">Donate</a>
-    </div>
+
+<section class="container becomeSponsor__container" id="becomeSponsor">
+  <div class="inner becomeSponsor__inner">
+    <h3 class="text-white">We made the world better with you.</h3>
+    <p class="lead text-white mt-3">Support all incredible impact makers by sponsoring this hackathon.</p>
+    <a class="becomeSponsor__cta" href="#register">
+      <button type="submit" class="btn-simple btn-lg btn-green btn-mobile">Sponsor the Fixathon</button>
+    </a>
   </div>
 </section>
 <!-- END BECOME SPONSOR -->
--- resources/views/partials/entries.blade.php
+++ resources/views/partials/entries.blade.php
@@ -1,178 +1,55 @@
 <!-- ENTRIES -->
-<section class="my-5 py-5 bg-light" id='entries'>
-  <div class="container py-3">
-    <div class="row text-center justify-content-center mb-3 pt-3">
-        <div class="col-8">
-          <h2>Entries</h2>
-          <p class="lead">
-          	Impact makers are building this amazing products to fight Climate breakdown. Check them & start building yours! 
-          </p>
-        </div>
+<div class="border-img__top border-img__top--blue"></div>
+<section class="container entries__container" id="entries">
+  <div class="inner twocol__inner entries__inner">
+    <div class="centered__header entries__header">
+      <h1 class="brush__title brush__title--large centered__title entries__title">
+        Entries
+      </h1>
+      <p class="centered__intro entries__intro">
+        Intro text
+      </p>
     </div>
-
-    <div class="row row-eq-height justify-content-center">
-      <div class="col-6 col-md-4 col-lg-3">
-        <div class="card shadow-sm shadow-lg--hover min-height-100 p-2">
-          <div class="row">
-            <div class="col-10 text-center m-auto py-2">
-              <img alt="image" class="img-fluid rounded" src="{{asset('img/sponsors/1.png')}}">
-            </div>
-
-            <div class="col-md-12 px-4">
-              <p class="h6 mt-3"><strong>Person Name</strong></p>
-              <p class="">
-                Even the all-powerful Pointing has no control about day however
-              </p>
-
-              <div class="row align-items-center my-3 pt-3 border-top mx-1">
-                <div class="col-sm-7 px-0">
-                  <div class="icon-actions">
-                    <a href="#" class="icon-action like active mr-2" style='text-decoration: none;'>
-                      <span style='filter: grayscale(100%);'>👍</span>
-                      <span class="text-muted">150</span>
-                    </a>
-                    <a href="#"  class="icon-action" style='text-decoration: none;'>
-                      <span style='filter: grayscale(100%);'>💬</span>
-                      <span class="text-muted">36</span>
-                    </a>
-                  </div>
-                </div>
-                <div class="col-sm-5 d-none d-sm-block pr-0">
-                  <div class="d-flex align-items-center justify-content-sm-end">
-                    <div class="avatar-group">
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Jessica Rowland">
-                        <img alt="Team" src="{{asset('img/people/1.jpg')}}" class="">
-                      </a>
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Audrey Love">
-                        <img alt="Team" src="{{asset('img/people/2.jpg')}}" class="rounded-circle">
-                      </a>
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Michael Lewis">
-                        <img alt="Team" src="{{asset('img/people/3.jpg')}}" class="rounded-circle">
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class='row mb-2'>
-				<a href="" class="btn btn-success btn-sm px-5 py-2 m-auto" style='opacity:0.75;'>View</a>
-              </div>
-
-            </div>
-          </div>
-        </div>
+    <div class="entries">
+    <div class="icon-actions">
+      <div>
+        <a href="#" class="icon-action like active mr-2" style='text-decoration: none;'>
+          <span style='filter: grayscale(100%);'>👍</span>
+          <span class="text-muted">150</span>
+        </a>
+        <a href="#"  class="icon-action" style='text-decoration: none;'>
+          <span style='filter: grayscale(100%);'>💬</span>
+          <span class="text-muted">36</span>
+        </a>
       </div>
-
-      <div class="col-6 col-md-4 col-lg-3">
-        <div class="card shadow-sm shadow-lg--hover min-height-100 p-2">
-          <div class="row">
-            <div class="col-10 text-center m-auto py-2">
-              <img alt="image" class="img-fluid rounded" src="{{asset('img/sponsors/2.png')}}">
-            </div>
-
-            <div class="col-md-12 px-4">
-              <p class="h6 mt-3"><strong>Person Name</strong></p>
-              <p class="">
-                "Far far away, behind the word mountains
-              </p>
-            </div>
-
-            <div class='d-flex flex-column-reverse col-12 px-4'>
-              <div class="row align-items-center my-3 pt-3 border-top mx-1">
-                <div class="col-sm-7 px-0">
-                  <div class="icon-actions">
-                    <a href="#" class="icon-action like active mr-2" style='text-decoration: none;'>
-                      <span style='filter: grayscale(100%);'>👍</span>
-                      <span class="text-muted">150</span>
-                    </a>
-                    <a href="#"  class="icon-action" style='text-decoration: none;'>
-                      <span style='filter: grayscale(100%);'>💬</span>
-                      <span class="text-muted">36</span>
-                    </a>
-                  </div>
-                </div>
-                <div class="col-sm-5 d-none d-sm-block pr-0">
-                  <div class="d-flex align-items-center justify-content-sm-end">
-                    <div class="avatar-group">
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Jessica Rowland">
-                        <img alt="Team" src="{{asset('img/people/1.jpg')}}" class="">
-                      </a>
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Audrey Love">
-                        <img alt="Team" src="{{asset('img/people/2.jpg')}}" class="rounded-circle">
-                      </a>
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Michael Lewis">
-                        <img alt="Team" src="{{asset('img/people/3.jpg')}}" class="rounded-circle">
-                      </a>
-                    </div>
-                  </div>
-                </div>
-
-	            <div class='col-12 mt-3 mb-2 text-center'>
-					<a href="" class="btn btn-success btn-sm px-5 py-2 m-auto" style='opacity:0.75;'>View</a>
-	            </div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="col-6 col-md-4 col-lg-3">
-        <div class="card shadow-sm shadow-lg--hover min-height-100 p-2">
-          <div class="row">
-            <div class="col-10 text-center m-auto py-2">
-              <img alt="image" class="img-fluid rounded" src="{{asset('img/sponsors/3.png')}}">
-            </div>
-
-            <div class="col-md-12 px-4">
-              <p class="h6 mt-3"><strong>Person Name</strong></p>
-              <p class="">
-                "Far far away, behind the word mountains, far from the countries
-              </p>
-
-              <div class="row align-items-center my-3 pt-3 border-top mx-1">
-                <div class="col-sm-7 px-0">
-                  <div class="icon-actions">
-                    <a href="#" class="icon-action like active mr-2" style='text-decoration: none;'>
-                      <span style='filter: grayscale(100%);'>👍</span>
-                      <span class="text-muted">150</span>
-                    </a>
-                    <a href="#"  class="icon-action" style='text-decoration: none;'>
-                      <span style='filter: grayscale(100%);'>💬</span>
-                      <span class="text-muted">36</span>
-                    </a>
-                  </div>
-                </div>
-                <div class="col-sm-5 d-none d-sm-block pr-0">
-                  <div class="d-flex align-items-center justify-content-sm-end">
-                    <div class="avatar-group">
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Jessica Rowland">
-                        <img alt="Team" src="{{asset('img/people/1.jpg')}}" class="">
-                      </a>
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Audrey Love">
-                        <img alt="Team" src="{{asset('img/people/2.jpg')}}" class="rounded-circle">
-                      </a>
-                      <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Michael Lewis">
-                        <img alt="Team" src="{{asset('img/people/3.jpg')}}" class="rounded-circle">
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <div class='row mb-2'>
-				<a href="" class="btn btn-success btn-sm px-5 py-2 m-auto" style='opacity:0.75;'>View</a>
-              </div>
-
-            </div>
-          </div>
+    </div>
+    <div class="col-sm-5 d-none d-sm-block pr-0">
+      <div class="d-flex align-items-center justify-content-sm-end">
+        <div class="avatar-group">
+          <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Jessica Rowland">
+            <img alt="Team" src="{{asset('img/people/1.jpg')}}" class="">
+          </a>
+          <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Audrey Love">
+            <img alt="Team" src="{{asset('img/people/2.jpg')}}" class="rounded-circle">
+          </a>
+          <a href="#" class="avatar avatar-xs rounded-circle" data-toggle="tooltip" data-original-title="Michael Lewis">
+            <img alt="Team" src="{{asset('img/people/3.jpg')}}" class="rounded-circle">
+          </a>
         </div>
       </div>
     </div>
 
-    <div class='row mt-5'>
-		<button class='btn btn-success m-auto col-4 mt-4'>Load More</button>
-    </div>
   </div>
 </section>
+<div class="border-img__bot border-img__bot--blue"></div>
+
+
+
+
+  
+
+
+
+
+
 <!-- END ENTRIES -->
