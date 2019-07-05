cf5e925ec807621862b987987e8a8f72fa666410
shylands <steve@shylands.com>
2019-07-05 17:06:43 +0200

    building footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,39 +1,37 @@
+<!-- <img alt="image" src="{{asset('img/logo_sm.png')}}" height="80" style='margin-top:-10px;'> -->
 
-
-<section class="container takepart__container" id="register">
-  <div class="inner twocol__inner takepart__inner">
-  <img alt="image" src="{{asset('img/logo_sm.png')}}" height="80" style='margin-top:-10px;'>
-  <div class=>
-    <p>An <a href="https://techimpactmakers.com">Impact Makers</a> project. Follow <a href="https://twitter.com/fixathon">@fixathon</a> on Twitter.</p>
-  </div>
-
-  <div class="">
-    © 2019 Impact Makers
-  </div>
-    <ul class="nav justify-content-center">
-      <li class="nav-item">
-        <a class="nav-link active text-dark" href="#about">About</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="#entries">Entries</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="privacity">Privacy Policy</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="terms">Terms</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link text-dark" href="about">About</a>
-      </li>
-    </ul>
-
-  
-</div>
-</section>
-
- 
-
+<footer class="container footer__container" id="footer">
+  <div class="inner footer__inner">
+    <div class="footer__top">
+      <div class="footer__im">
+        <h3 class="footer__im-title">An Impact Makers project from:</h3>
+        <ul class="footer__im-from">
+          <li><a href="https://twitter.com/shylands" target='_blank'>        <img alt="Follow @shylands Twitter" class="footer__im-from-avatar" src="{{asset('img/people/steven.jpg')}}"> @shylands</a></li>
+          <li><a href="https://twitter.com/jp_aulet" target='_blank'>        <img alt="Follow @jp_aulet Twitter" class="footer__im-from-avatar" src="{{asset('img/people/jp.jpg')}}"> @jp_aulet</a></li>
+        </ul>
+        <p class="footer__im-p">Impact Makers is a community of tech people who want to help fix the climate.</p>
+        <div class="footer__im-actions">
+        <a class="footer__im-action footer__im-action--primary" href="https://techimpactmakers.com" target='_blank'>
+          <button type="submit" class="btn-simple btn-sm btn-green btn-mobile">Learn more</button>
+        </a>
+        <a class="footer__im-action footer__im-action--secondary" href="https://join.slack.com/t/impact-makers-group/shared_invite/enQtNjQ2MTY3NDM1MjcxLWUwNjA1YWViZmI1NjQ5YTIzMjA1OWE2NzI3NzMwNzQ0ODBiMmZlMzI0YTI4MDUyMzlhNmZhZDFiOWZkODQ0Yjg" target='_blank'>
+          <button type="submit" class="btn-bordered btn-sm btn-secondary btn-mobile">Join our Slack</button>
+        </a>
+        </div>
+      </div>
+      <ul class="footer__nav">
+        <li><a href="/faq">FAQs</a></li>
+        <li><a href="mailto:hi@fixathon.io">Email us</a></li>
+        <li class="footer__nav-twitter"><a href="https://twitter.com/fixathon" target='_blank'>        <img alt="Follow on Twitter" class="judge__twitter-icon" src="{{asset('img/icons/twitter.svg')}}"> @fixathon</a></li>
+      </ul>
+    </div>
+    <div class="footer__bot">
+      <span class="footer__with">
+        In association with <a href="https://getmakerlog.com" target='_blank'>Makerlog</a> and <a href="https://makerpad.co" target='_blank'>Makerpad</a>.
+      </span>
+      <span class="footer__statement">
+        © 2019 Impact Makers
+      </span>
     </div>
   </div>
 </footer>
