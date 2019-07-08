342394f2e5ea8f0e5f62553780beeec7fad7ff50
shylands <steve@shylands.com>
2019-07-08 16:39:51 +0200

    content and style tweaks

--- resources/views/partials/registrationBanner.blade.php
+++ resources/views/partials/registrationBanner.blade.php
@@ -4,7 +4,7 @@
 <section class="container regbanner__container" id="register-banner">
   <div class="inner regbanner__inner">
     <h1 class="regbanner__title">
-      Have an idea that you like to enter? Join us!
+      Want to partcipate in the Fixathon?
     </h1>
     <a class="regbanner__cta" href="#register">
       <button type="submit" class="btn-simple btn-md btn-white-blue btn-mobile">Register Now</button>
