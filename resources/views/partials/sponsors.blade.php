fffad0e0714b0ef22c27478ffcdebcf575a4ac55
shylands <steve@shylands.com>
2019-07-09 18:09:38 +0200

    building sponsor section

--- resources/views/partials/sponsors.blade.php
+++ resources/views/partials/sponsors.blade.php
@@ -13,7 +13,89 @@
       </p>
     </div>
     <div class="sponsors">
-      <img alt="image" class="" src="{{asset('img/sponsors/1.png')}}">
+      <ul class="sponsors__silver">
+        <li class="sponsor__silver">
+          <a href='https://1password.com' title='1Password' target='_blank'>
+            <img alt="1Password" src="{{asset('img/sponsors/1password.png')}}">
+          </a>
+          <p>The best way to keep you and your family, team, and business safe online. Go ahead. Forget your passwords.</p>
+          <a href='https://1password.com' title='1Password' target='_blank'>1password.com</a>  
+        </li>
+        <li class="sponsor__silver">
+          <a href='https://sketch.com' title='Sketch' target='_blank'>
+            <img alt="Sketch" src="{{asset('img/sponsors/sketch.png')}}">
+          </a>
+          <p>Over one million designers use Sketch to transform their ideas into incredible products, every day.</p>
+          <a href='https://sketch.com' title='Sketch' target='_blank'>sketch.com</a>  
+        </li>
+        <li class="sponsor__silver">
+          <a href='https://egghead.io' title='Egghead.io' target='_blank'>
+            <img alt="Egghead.io" src="{{asset('img/sponsors/egghead.png')}}">
+          </a>
+          <p>High quality, concise web development screencasts. Coding with expert open-source contributors.</p>
+          <a href='https://egghead.io' title='Egghead.io' target='_blank'>egghead.io</a>  
+        </li>
+      </ul>
+      <ul class="sponsors__bronze">
+        <li class="sponsor__bronze">
+          <a href='https://slite.com/' title='Slite' target='_blank'>
+            <img alt="Slite" src="{{asset('img/sponsors/slite.png')}}">
+          </a>
+        </li>
+        <li class="sponsor__bronze">
+          <a href='https://greengovernance.org/' title='Green Governance' target='_blank'>
+            <img alt="Green Governance" src="{{asset('img/sponsors/greengovernance.png')}}">
+          </a>
+        </li>
+        <li class="sponsor__bronze">
+          <a href='https://marvelapp.com/' title='Marvel' target='_blank'>
+            <img alt="Marvel" src="{{asset('img/sponsors/marvel.png')}}">
+          </a>
+        </li>    
+        <li class="sponsor__bronze">
+          <a href='https://bubble.is/' title='Bubble' target='_blank'>
+            <img alt="Bubble" src="{{asset('img/sponsors/bubble.png')}}">
+          </a>
+        </li>
+        <li class="sponsor__bronze">
+          <a href='https://getkirby.com/' title='Kirby' target='_blank'>
+            <img alt="Kirby" src="{{asset('img/sponsors/kirby.png')}}">
+          </a>
+        </li>
+        <li class="sponsor__bronze">
+          <a href='https://referralhero.com/' title='ReferralHero' target='_blank'>
+            <img alt="ReferralHero" src="{{asset('img/sponsors/referralhero.png')}}">
+          </a>
+        </li>
+        <li class="sponsor__bronze">
+          <a href='https://drawkit.com/' title='DrawKit' target='_blank'>
+            <img alt="DrawKit" src="{{asset('img/sponsors/drawkit.png')}}">
+          </a>
+        </li> 
+        <li class="sponsor__bronze">
+          <a href='https://offset.earth/' title='Offset Earth' target='_blank'>
+            <img alt="Offset Earth" src="{{asset('img/sponsors/offsetearth.png')}}">
+          </a>
+        </li> 
+        <li class="sponsor__bronze">
+          <a href='https://getyomo.com/' title='Yomo' target='_blank'>
+            <img alt="Yomo" src="{{asset('img/sponsors/yomo.png')}}">
+          </a>
+        </li>
+        <li class="sponsor__bronze">
+          <a href='https://hotjar.com/' title='Hotjar' target='_blank'>
+            <img alt="Hotjar" src="{{asset('img/sponsors/hotjar.png')}}">
+          </a>
+        </li>  
+
+        <li class="sponsor__bronze">
+          <a href='https://framer.com/' title='Framer' target='_blank'>
+            <img alt="framer" src="{{asset('img/sponsors/framer.png')}}">
+          </a>
+        </li>  
+      </ul>
+
+
     </div>
   </div>
 </section>
