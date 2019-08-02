dc570416ce58c4c3d5588814f73201b35934ec47
jp aulet <blakdrak@gmail.com>
2019-08-02 06:34:19 +0200

    Moved the countdown to PT timezone

--- resources/views/partials/introLoggedin.php
+++ resources/views/partials/introLoggedin.php
@@ -90,18 +90,18 @@
 
 <script>
   const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
-  let countDown = new Date('Aug 30, 2019 00:00:00').getTime(),
-    x = setInterval(function() {
-        let now = new Date().getTime(),
-        distance = countDown - now;
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  let countDown = new Date(Date.UTC('2019', '07', '30', '07', '00', '00')).getTime(),x = setInterval(function() {
+    let now = new Date().getTime(),
+    distance = countDown - now;
     document.getElementById('days').innerText = Math.floor(distance / (day)),
         document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
         document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
         document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
-    }, second)
+  }, second)
 </script>
 
 <!-- END INTRO -->
