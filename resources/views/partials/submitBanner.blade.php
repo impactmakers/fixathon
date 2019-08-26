f58706ee118aaa10324767dd510615e560be9da5
jp aulet <blakdrak@gmail.com>
2019-08-26 06:47:50 +0200

    Solved date typo

--- resources/views/partials/submitBanner.blade.php
+++ resources/views/partials/submitBanner.blade.php
@@ -3,12 +3,11 @@
 <section class="container submit__container" id="register-banner">
   <div class="inner submit__inner">
     <span class="submit__title">
-      Submit your project by 23:59 PT on August 1st!
+      Submit your project by 23:59 PT on August 30th!
       <a href="https://forms.gle/USjZHhi2KbM2xztJ9">
       Submit project now
       </a>
     </span>
-
   </div>
 </section>
 <div class="border-img__top border-img__top--blue-yellow"></div>
